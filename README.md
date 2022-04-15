afa00db8fd5ca42c409d7237cbb40cc2f9da900b
Jose Galdamez <josegaldamez1991@gmail.com>
2022-04-14 22:38:27 -0600

    First commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Speech to Text
+A little example about how to use the Speech to Text in Flutter.
---
+++ android/app/src/main/kotlin/com/codigocorrecto/audiototext/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.codigocorrecto.audiototext
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
